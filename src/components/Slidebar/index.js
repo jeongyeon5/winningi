1d2b2a06195bd2cd4ac8eef63ac5bf5251ae4d6b
jeongyeon <jeongyeon@MacBookPro.local>
2022-10-20 09:47:09 +0900

    [Add] slidebar click 이벤트 추가

--- src/components/Slidebar/index.js
+++ src/components/Slidebar/index.js
@@ -2,39 +2,55 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { FaUserAlt, FaClipboardList } from 'react-icons/fa';
 import { AiTwotoneSetting } from 'react-icons/ai';
-import { CgLogOff } from 'react-icons/cg';
+import { CgLogOff, CgMenu, CgClose } from 'react-icons/cg';
+import { useState } from 'react';
 
 const Slidebar = () => {
+  const [slide, setSlide] = useState(false);
+  const handleSlide = () => {
+    setSlide(!slide);
+  };
   return (
     <>
       <SlideWrap>
-        <div className='slideInner'>
-          <div className='menuList'>
-            <Link to={'/'} className='menu'>
-              <FaUserAlt size='30' />
-              User
-            </Link>
-            <div className='nav'>
-              <Link to={'/board'} className='menu'>
-                <FaClipboardList size='30' /> 게시판
+        <CgMenu className='slideMenuIcon' size='30' onClick={handleSlide} />
+        {slide && (
+          <div className='slideInner' slide={!slide}>
+            <CgClose size='30' className='closeBtn' onClick={() => setSlide(false)} />
+            <div className='menuList'>
+              <Link to={'/'} className='menu'>
+                <FaUserAlt size='30' />
+                User
               </Link>
+              <div className='nav'>
+                <Link to={'/board'} className='menu'>
+                  <FaClipboardList size='30' /> 게시판
+                </Link>
+                <div className='menu'>
+                  <AiTwotoneSetting size='30' /> 개인설정
+                </div>
+              </div>
               <div className='menu'>
-                <AiTwotoneSetting size='30' /> 개인설정
+                <CgLogOff size='30' />
+                로그아웃
               </div>
             </div>
-            <div className='menu'>
-              <CgLogOff size='30' />
-              로그아웃
-            </div>
           </div>
-        </div>
+        )}
       </SlideWrap>
     </>
   );
 };
 const SlideWrap = styled.div`
   position: relative;
+  .slideMenuIcon {
+    position: absolute;
+    top: 30px;
+    left: 30px;
+    color: #3098e8;
+  }
   .slideInner {
+    transition: all 0.7s;
     background-color: #ddd;
     position: fixed;
     top: 0;
@@ -42,11 +58,17 @@ const SlideWrap = styled.div`
     width: 200px;
     height: 100%;
     padding: 100px 20px;
+    .closeBtn {
+      position: absolute;
+      top: 30px;
+      left: 130px;
+    }
     .menuList {
       display: flex;
       flex-direction: column;
       justify-content: space-between;
       height: 100%;
+
       .nav {
         display: flex;
         flex-direction: column;
